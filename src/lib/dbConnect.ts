0a7c5fe5a530631c3c8d5f2bb13e6a7b3a303f14
Shubham kahar <Shubhamkahar196@gmail.com>
2025-09-13 15:36:53 +0530

    added email helper and apiResponse

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,4 +1,4 @@
-import mongoose, { NumberExpression }  from "mongoose";
+import mongoose  from "mongoose";
 
 type ConnectionObject ={
     isConnected?: number;
@@ -13,9 +13,14 @@ async function connectDb(): Promise<void>{
      }
 
      try{
-
+       const db = await mongoose.connect(process.env.MONGODB_URL || '')
+       
+      connection.isConnected= db.connections[0].readyState
+      console.log("DB connected successfully")
      }catch(err){
         console.log("Error while connecting to db",err);
         process.exit(1);
      }
 }
+
+export default connectDb;
